f50ad4988abc6b96469268de4a0e1df61ed62535
Sushi-UX <thechoygroup@gmail.com>
2019-07-02 15:36:20 -0400

    Added Project-02 to Github to reconnect repository
    Yesterday X-code similator was taking too much memory so I force quit GitHub which I fear was no longer saving changes of my project-02. So I am adding again to Git-Hub to make sure changes are saved.

---
+++ EnhancedQuizApp/EnhanceQuizStarter/EnhanceQuizStarter/PhraseProvider.swift
@@ -0,0 +1,16 @@
+//
+//  PhraseProvider.swift
+//  EnhanceQuizStarter
+//
+//  Created by Raymond Choy on 7/1/19.
+//  Copyright © 2019 Treehouse. All rights reserved.
+//
+
+import UIKit
+import GameKit
+
+struct PhraseProvider {
+    let phrases = [
+        (Question: )
+    ]
+}
